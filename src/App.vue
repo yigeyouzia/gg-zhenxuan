6693d95d95647ec2a2aaaf4d8b12fba18e032ba7
cyt-cyt <10535337+cyt-cyt@user.noreply.gitee.com>
2023-07-29 17:25:54 +0800

    feat: 所有项目准备 scss axios二次封装 svg element ui, 组件注册

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>我是app</div>
+  <div>
+    <p>我是app</p>
+  </div>
+  <SvgIcon name="phone" color="pink" width="130px" height="130px"></SvgIcon>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reqLogin } from "@/api/user"
+import { onMounted } from 'vue';
 
-<style lang="scss"></style>
+let f = async () => {
+  let res = await reqLogin({
+    username: 'admin',
+    password: '111111'
+  })
+  console.log(res);
+
+}
+onMounted(() => {
+  f()
+})
+</script>
+
+<style scoped lang="scss">
+div {
+  height: 1111px;
+
+  p {
+    color: red;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 
-createApp(App).mount('#app')
+// 1element ui
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs' // 国际化
+
+// 2获取应用实例对象
+const app = createApp(App)
+
+// 3安装ele
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+
+// 4svg 插件
+import 'virtual:svg-icons-register'
+// 5引入自定义插件对象，注册全局组件
+import globalComponent from '@/components'
+// 安装自定义插件
+app.use(globalComponent)
+
+//6.引入全局样式
+import '@/styles/index.scss'
+
+app.mount('#app')
