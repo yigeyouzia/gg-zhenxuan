3a613c99beb9f460465b3a219b1b59cd4be72ff1
cyt-cyt <10535337+cyt-cyt@user.noreply.gitee.com>
2023-07-30 23:45:16 +0800

    feat: views文件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,7 @@
 <template>
-    <div>我是一级路由home</div>
+  <div>我是一级路由home</div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss"></style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,31 +1,45 @@
 <template>
-    <div class="login-container">
-        <el-row>
-            <el-col :span="12" :xs="0"></el-col>
-            <el-col :span="12" :xs="24">
-                <el-form class="login-form">
-                    <h1>hello</h1>
-                    <h2>欢迎来到✨✨✨</h2>
-                    <el-form-item label="" size="normal">
-                        <el-input prefix-icon="User" v-model="loginForm.username">
-                        </el-input>
-                    </el-form-item>
-                    <el-form-item label="" size="normal">
-                        <el-input type="password" prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
-                    </el-form-item>
-                    <el-form-item label="" size="normal">
-                        <el-button :loading="loading" class="login-btn" type="primary" size="default"
-                            @click="login">登录</el-button>
-                    </el-form-item>
-                </el-form>
-            </el-col>
-        </el-row>
-    </div>
+  <div class="login-container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login-form">
+          <h1>hello</h1>
+          <h2>欢迎来到✨✨✨</h2>
+          <el-form-item label="" size="normal">
+            <el-input
+              prefix-icon="User"
+              v-model="loginForm.username"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="" size="normal">
+            <el-input
+              type="password"
+              prefix-icon="Lock"
+              v-model="loginForm.password"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="" size="normal">
+            <el-button
+              :loading="loading"
+              class="login-btn"
+              type="primary"
+              size="default"
+              @click="login"
+            >
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "vue"
-import { useRouter } from "vue-router";
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { ElNotification } from 'element-plus'
 
 // 引入user小仓库
@@ -40,62 +54,61 @@ let loading = ref(false)
 let loginForm = reactive({ username: 'admin', password: '111111' })
 // 登录按钮回调
 const login = async () => {
-    // 加载效果
-    loading.value = true
-    try {
-        await userStore.userLogin(loginForm)
-        // 跳转到首页
-        $router.push('/')
-        ElNotification({
-            type: 'success',
-            message: "成功"
-        })
-    } catch (error) {
-        ElNotification({
-            type: 'error',
-            message: (error as Error).message
-        })
-    } finally {
-        loading.value = false
-    }
+  // 加载效果
+  loading.value = true
+  try {
+    await userStore.userLogin(loginForm)
+    // 跳转到首页
+    $router.push('/')
+    ElNotification({
+      type: 'success',
+      message: '成功',
+    })
+  } catch (error) {
+    ElNotification({
+      type: 'error',
+      message: (error as Error).message,
+    })
+  } finally {
+    loading.value = false
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .login-container {
-    width: 100%;
-    height: 100vh;
-    background: linear-gradient(to bottom,
-            rgba(110, 116, 130, 0.5),
-            rgba(89, 86, 87, 0.5)), url('@/assets/images/background1.jpg') center/80% no-repeat;
-    // background: content-box radial-gradient(rgb(143, 74, 88), rgb(89, 135, 152));
-    background: url('@/assets/images/background.jpg') center/80% no-repeat;
+  width: 100%;
+  height: 100vh;
+  background:
+    linear-gradient(to bottom, rgba(110, 116, 130, 0.5), rgba(89, 86, 87, 0.5)),
+    url('@/assets/images/background1.jpg') center/80% no-repeat;
+  // background: content-box radial-gradient(rgb(143, 74, 88), rgb(89, 135, 152));
+  background: url('@/assets/images/background.jpg') center/80% no-repeat;
+  background-size: cover;
+
+  .login-form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    right: 10vh;
+    background: url('@/assets/images/login_form.png') no-repeat;
     background-size: cover;
+    padding: 40px;
 
-    .login-form {
-        position: relative;
-        width: 80%;
-        top: 30vh;
-        right: 10vh;
-        background: url('@/assets/images/login_form.png') no-repeat;
-        background-size: cover;
-        padding: 40px;
-
-        h1 {
-            color: #fff;
-            font-size: 40px;
-        }
-
-        h2 {
-            color: #fff;
-            font-size: 20px;
-            margin: 20px 0;
-        }
-
-        .login-btn {
-            width: 100%;
-        }
+    h1 {
+      color: #fff;
+      font-size: 40px;
     }
+
+    h2 {
+      color: #fff;
+      font-size: 20px;
+      margin: 20px 0;
+    }
+
+    .login-btn {
+      width: 100%;
+    }
+  }
 }
 </style>
- 
